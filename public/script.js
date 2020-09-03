75b011e49222d5e97b69a2e5788bf81deb1dc760
Borges <gustavoborges98@hotmail.com>
2020-09-03 01:00:01 -0300

    rotas e scirpt js

--- public/script.js
+++ public/script.js
@@ -1,15 +1,8 @@
-const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card');
 
 for (let card of cards) {
     card.addEventListener("click", function() {
-        const pageId = card.getAttribute("id")
-        modalOverlay.classList.add('active')
-        modalOverlay.querySelector("iframe").src= `modals/${pageId}.html`;
-
+        const receitaId = card.getAttribute("id")
+        window.location.href = `/recipes?id=${receitaId}`
     })
 }
-
-document.querySelector(".close-modal").addEventListener("click", function() {
-    modalOverlay.classList.remove("active")
-})
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
-const receitas = require("./data")
+const recipes = require("./data")
 
 server.use(express.static('public'))
 
@@ -19,19 +19,38 @@ server.get("/", function(req, res) {
     return res.render("home")
 })
 
-server.get("/receitas", function(req, res) {
+server.get("/recipes", function(req, res) {
 
-    return res.render("receitas")
+    return res.render("recipes")
 })
 
-server.get("/sobre", function(res, res) {
-    return res.render("sobre")
+server.get("/recipes/:id", function(req, res) {
+    const id = req.query.id
+
+    const recipe = recipes.find(function(recipe) {
+        return recipe.title == id
+    })
+
+    if (!recipe) {
+        return res.send("Receita não encontrada!")
+    }
+
+    return res.render("recipe", {item: recipe})
+
+    // const recipeIndex = req.params.index
+    // console.log(recipes[recipeIndex])
+    // return res.render(recipes[recipeIndex])
+    // return res.render("recipe")
+})
+
+server.get("/about", function(res, res) {
+    return res.render("about")
 })
 
 
-// server.use(function(req, res) {
-//     res.status(404).render("not-found")
-// })
+server.use(function(req, res) {
+    res.status(404).render("not-found")
+})
 
 server.listen(5000, function() {
     console.log("server is running...")
